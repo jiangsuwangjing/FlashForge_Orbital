4c0d557278fe9b19bd0adc744a6b0fdc950cbcce
jiangsuwangjing <153513738+jiangsuwangjing@users.noreply.github.com>
2024-06-02 20:26:25 +0800

    Add files via upload

---
+++ src/registration/Login.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import '../styles/Login.css'
+import logo from '../assets/logo.png'
+
+export const Login = (props) => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+    handleSignup,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+    signInWithGoogle
+  } = props;
+  return (
+    <div className="base-container">
+      <div className="header">LogIn</div>
+      <div className="content">
+        <div className="image">
+          <img src={logo}/>
+        </div>
+        <div className="form">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              placeholder="username"
+              autoFocus
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p className="errorMsg">{emailError}</p>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p className="errorMsg">{passwordError}</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="footer">
+          {hasAccount ? (
+            <>
+              <button className="btn" onClick={handleLogin}>Sign in</button>
+              <p>Have an account ? 
+                <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+              </p>
+            </>
+            ) : (
+            <>
+              <button onClick={handleSignup} className="btn" >Register</button>
+              <p>Don't have an account ? 
+                <span onClick={() => setHasAccount(!hasAccount) }>Register</span>
+              </p>
+            </>
+          )}
+        </div>
+        <button onClick={signInWithGoogle}>Sign In With Google</button>
+      </div>
+    </div>
+  )
+}
