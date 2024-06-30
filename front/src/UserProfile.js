081792c19659f9d47803fa3a116c7ed2f524b526
jiangsuwangjing <153513738+jiangsuwangjing@users.noreply.github.com>
2024-07-01 01:31:51 +0800

    Delete front directory

--- front/src/UserProfile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./styles/UserProfile.css";
-export default function UserProfile() {
-  return (
-    <div>
-      <div className="hero"></div>
-    </div>
-  );
-}
